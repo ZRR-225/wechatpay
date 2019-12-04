3342c279f6cffc000c28d49738c9998b3a9a079f
ZRR <1696387642@qq.com>
2019-12-04 14:13:57 +0800

    第一次提交

---
+++ yangzheng/gzh/js/main.js
@@ -0,0 +1,8 @@
+	function showDetails(number) {
+	    var numberType = number.getAttribute("data-num");
+		$(".submit").click(function(){
+			p1=numberType
+			console.log(p1)
+			var mobile=$(".prependedInput").val();
+		})
+	}
